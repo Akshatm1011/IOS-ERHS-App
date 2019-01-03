1118953c5ce1a5886d963a3ba84393ddc567321b
Akshat Muir <akshatm1011@gmail.com>
2019-01-02 16:52:39 -0800

    Initial Commit

---
+++ ERHS AppTests/ERHS_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  ERHS_AppTests.swift
+//  ERHS AppTests
+//
+//  Created by Akshat Muir on 1/2/19.
+//  Copyright © 2019 Akshat Muir. All rights reserved.
+//
+
+import XCTest
+@testable import ERHS_App
+
+class ERHS_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
